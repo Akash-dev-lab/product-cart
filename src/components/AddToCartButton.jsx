92ff19ec3a30b6698519abb5487d62ead4bccb74
Akash God <111779287+Akash-dev-lab@users.noreply.github.com>
2024-11-16 11:23:46 +0000

    minor changes

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -34,13 +34,13 @@ const AddToCartButton = ({ item }) => {
     };
 
     return (
-        <div className='absolute bottom-20 left-14'>
+        <div className='absolute max-2xl:absolute max-xl:absolute max-lg:absolute max-md:absolute max-sm:absolute max-sm:left-14 max-md:left-44 max-md:bottom-20 max-sm:bottom-20 max-lg:bottom-28 max-lg:left-2 bottom-20 left-20 max-xl:left-3 max-2xl:left-10'>
         {quantity === 0 ? (
             <button
-                className="add-to-cart-button w-56 flex gap-2 justify-center items-center border-[#562121] text-[#2E0F0F] border font-[akash] rounded-full py-2 px-4 cursor-pointer font-semibold bg-rose-50 hover:bg-[#eaeaea]"
+                className="add-to-cart-button lg:w-44 lg:p-5 md:w-32 md:text-sm md:h-8 max-md:w-52 flex gap-2 justify-center items-center border-[#562121] text-[#2E0F0F] border font-[akash] rounded-full py-2 max-md:px-4 cursor-pointer font-semibold bg-rose-50 hover:bg-[#eaeaea]"
                 onClick={handleAddToCart}
             >
-                <span>
+                <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
                         <g fill="#C73B0F" clipPath="url(#a)">
                             <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
@@ -52,23 +52,23 @@ const AddToCartButton = ({ item }) => {
                             </clipPath>
                         </defs>
                     </svg>
-                </span>
-                Add to Cart
+                    </div>
+                <div className=''>Add to Cart</div>
             </button>
             ) : (
-                <div className="flex w-52 items-center gap-8 py-2 px-4 justify-between text-[#2E0F0F] font-[akash] rounded-full bg-[#D35400]">
+                <div className="flex max-2xl:w-44 w-44 max-xl:w-44 max-xl:flex max-sm:w-52 items-center max-md:w-52 max-md:text-sm max-lg:w-32 gap-8 py-2 px-4 justify-between text-[#2E0F0F] font-[akash] rounded-full bg-[#D35400]">
                     <button
-                        className="border w-5 h-5 rounded-full p-1"
+                        className="border flex justify-center items-center w-6 h-6 max-2xl:flex max-2xl:justify-center max-2xl:items-center max-xl:flex max-xl:justify-center max-xl:w-6 max-xl:h-6 max-xl:items-center max-2xl:w-6 max-2xl:h-6 max-lg:flex max-lg:justify-center max-lg:items-center max-md:w-6 max-md:h-6  rounded-full p-1"
                         onClick={decreaseQuantity}
                     >
-                        <img className='w-5' src="/assets/images/icon-decrement-quantity.svg" alt="" />
+                        <img className='max-xl:w-6 max-lg:w-6 max-2xl:w-6 max-2xl:h-6 max-lg:w-6 max-lg:h-6' src="/assets/images/icon-decrement-quantity.svg" alt="" />
                     </button>
-                    <span className="text-lg font-[akash] text-white font-semibold">{quantity}</span>
+                    <span className="text-lg max-lg:text-base font-[akash] text-white font-semibold">{quantity}</span>
                     <button
                         className="border rounded-full p-1"
                         onClick={increaseQuantity}
                     >
-                        <img className='w-3' src="/assets/images/icon-increment-quantity.svg" alt="" />
+                        <img className='w-4' src="/assets/images/icon-increment-quantity.svg" alt="" />
                     </button>
                 </div>
             )}
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,17 +6,15 @@ const ProductCard = ({ item }) => {
     const imageSrc = GetImageSource(item.image);
 
     return (
-        
-            <div className="product-card relative font-[akash]">
+            <div className="product-card relative sm:relative md:relative font-[akash]">
                 <img src={imageSrc} alt={item.name} className="w-screen object-cover rounded-md mb-4" />
                 <div className='pt-4'>
                 <p className='text-[#785E57] text-base'>{item.category}</p>
                 <h3>{item.name}</h3>
-                <p>Price: ${item.price.toFixed(2)}</p>
+                <p className='text-[#D35400] font-semibold'>${item.price.toFixed(2)}</p>
                 <AddToCartButton item={item} />
                 </div>
             </div>
-
     );
 };
 
